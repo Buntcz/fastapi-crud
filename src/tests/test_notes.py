fe5ff189d63087db716fdf3fb6a992bfcd64fab8
Bozhidar <tilevbojidar@gmail.com>
2026-06-30 01:11:28 +0300

    Added Additional Validation to all methods from CRUD

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -22,6 +22,9 @@ def test_create_note_invalid_json(test_app):
     response = test_app.post("/notes/", content=json.dumps({"title": "something"}))
     assert response.status_code == 422
 
+    response = test_app.post("/notes/", content=json.dumps({"title": 1, "description": "2"}))
+    assert response.status_code == 422
+
 def test_read_note(test_app, monkeypatch):
     test_data = {"id": 1, "title":"something", "description": "something else"}
 
@@ -43,6 +46,9 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
     response = test_app.get("/notes/999")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.get("/notes/0")
+    assert response.status_code == 422
 
 
 def test_read_all_notes(test_app, monkeypatch):
@@ -83,7 +89,10 @@ def test_update_note(test_app, monkeypatch):
     [
      [1,{},422],
      [1,{"description": "bar"}, 422],
-     [999, {"title": "foo", "description": "bar"}, 404]   
+     [999, {"title": "foo", "description": "bar"}, 404],
+     [1, {"title": "foo", "description": 1}, 422],
+     [1, {"title": "foo", "description": 1}, 422],
+     [0, {"title": "foo", "description": "bar"}, 422],
     ],
 )
 def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
@@ -121,5 +130,8 @@ def test_remove_note_invalid(test_app, monkeypatch):
     response = test_app.delete("/notes/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
     
 
